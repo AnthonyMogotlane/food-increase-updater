2e05de4df86b7390967df4713076a1cadc65e9e1
Phumza kose <kosephumza@gmail.com>
2022-06-08 16:19:52 +0200

    added a dropdown in html

--- js/price-checker.dom.js
+++ js/price-checker.dom.js
@@ -1,46 +1,42 @@
-
-let foodItem = document.querySelector("#foodItem");
-let searchBtn = document.querySelector(".search-btn");
+let dropdown = document.querySelector(".food-items");
+console.log(dropdown.options[dropdown.selectedIndex].value);
 
 let test = PriceChecker();
 
+dropdown.addEventListener("change", (e) => {
+  e.preventDefault();
 
-searchBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    test.setProductDetails(foodItem.value);
-    
-    let theObj = test.getProductObj();
-    
-    const labels = [
-        '2017',
-        '2018',
-        '2019',
-        '2020',
-        '2021',
-      ];
-    
-      const data = {
-        labels: labels,
-        datasets: [{
-          label: 'My First dataset',
-          backgroundColor: 'rgb(0, 0, 132)',
-          borderColor: 'rgb(0, 99, 132)',
-          data: [theObj[2017], theObj[2018], theObj[2019], theObj[2020], theObj[2021]],
-        }]
-      };
-    
-      const config = {
-        type: 'bar',
-        data: data,
-        options: {
-            maintainAspectRatio: false
-        }
-      };
-    
-    
-      const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-      );
+  test.setProductDetails(dropdown.options[dropdown.selectedIndex].value);
 
-})
+  let theObj = test.getProductObj();
+
+  const labels = ["2017", "2018", "2019", "2020", "2021"];
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(0, 0, 132)",
+        borderColor: "rgb(0, 99, 132)",
+        data: [
+          theObj[2017],
+          theObj[2018],
+          theObj[2019],
+          theObj[2020],
+          theObj[2021],
+        ],
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      maintainAspectRatio: false,
+    },
+  };
+
+  const myChart = new Chart(document.getElementById("myChart"), config);
+});
