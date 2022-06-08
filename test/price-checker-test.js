66fe5d2a02c7267a339f84cee7496620ddb51339
Nomzamo <nomzamo.nmuleka@gmail.com>
2022-06-08 10:22:23 +0200

    Added simple-grid file

--- test/price-checker-test.js
+++ test/price-checker-test.js
@@ -4,10 +4,43 @@ describe('The price checker app', function() {
 
 		const priceChecker = PriceChecker();
 
+		priceChecker.selectProduct();
+		
+
+		assert.equal("Cooking Oil", priceChecker.addProduct());
+
+	});
+
+    it ('should be able add the product', function() {
+
+		const priceChecker = PriceChecker();
+
+		priceChecker.addProduct();
+		priceChecker.addProduct();
+
+		assert.equal(78.22, priceChecker.totalProducts());
+
+	});
+    
+    it ('should be able add the product', function() {
+
+		const priceChecker = PriceChecker();
+
+		priceChecker.addProduct();
+		priceChecker.addProduct();
+
+		assert.equal(78.22, priceChecker.totalProducts());
+
+	});
+
+    it ('should bring back an error message "No data entered" when data is not enetered', function() {
+
+		const priceChecker = PriceChecker();
+
 		priceChecker.addProduct();
 		
 
-		assert.equal(5, priceChecker.addProduct());
+		assert.equal("No data entered", priceChecker.totalProducts());
 
 	});
 
