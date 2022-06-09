277c7a01ab436bafb626d381fdaa8d81c9c3a19c
Anthony Mogotlane <almogotlane@gmail.com>
2022-06-09 02:35:10 +0200

    Modified logic

--- js/price-checker.dom.js
+++ js/price-checker.dom.js
@@ -1,22 +1,33 @@
+//DOM element references
 let dropdown = document.querySelector(".food-items");
-console.log(dropdown.options[dropdown.selectedIndex].value);
+let canvasContainer = document.querySelector(".canvas-container");
+let canvasChart = document.querySelector("#myChart");
 
+
+//instance of PriceChecker function
 let test = PriceChecker();
 
+//dropdown event listener
 dropdown.addEventListener("change", (e) => {
   e.preventDefault();
 
+  //dropdown input value
   test.setProductDetails(dropdown.options[dropdown.selectedIndex].value);
 
-  let theObj = test.getProductObj();
+  let theObj = test.getProductObj(); //selected object from the list of products
 
+  //remove and create canvas element
+  canvasChart.remove();
+  canvasContainer.innerHTML = `<canvas id="myChart"></canvas>`
+
+  //product graph set up
   const labels = ["2017", "2018", "2019", "2020", "2021"];
 
   const data = {
     labels: labels,
     datasets: [
       {
-        label: "My First dataset",
+        label: `${theObj.itemName}`,
         backgroundColor: "rgb(0, 0, 132)",
         borderColor: "rgb(0, 99, 132)",
         data: [
@@ -29,9 +40,8 @@ dropdown.addEventListener("change", (e) => {
       },
     ],
   };
-
   const config = {
-    type: "bar",
+    type: "line",
     data: data,
     options: {
       maintainAspectRatio: false,
@@ -40,3 +50,12 @@ dropdown.addEventListener("change", (e) => {
 
   const myChart = new Chart(document.getElementById("myChart"), config);
 });
+
+
+
+
+
+
+
+
+
